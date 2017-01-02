5c2cbfdec3ffb508f811803f81491322148a711c
Olivier Dolbeau <contact@odolbeau.fr>
2017-01-02 17:39:56 +0100

    Remove cannotBeEmpty call on integernode

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -26,7 +26,7 @@ class Configuration implements ConfigurationInterface
                     ->prototype('array')
                         ->children()
                             ->scalarNode('host')->defaultValue('127.0.0.1')->cannotBeEmpty()->end()
-                            ->integerNode('port')->defaultValue(6379)->cannotBeEmpty()->end()
+                            ->integerNode('port')->defaultValue(6379)->end()
                             ->integerNode('base')->treatNullLike(0)->defaultValue(0)->end()
                         ->end()
                     ->end()
