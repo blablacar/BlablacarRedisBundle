25b6acd7478519539e949597b04075e0f1799cef
Edouard Durieux <edouard.durieux@blablacar.com>
2018-12-04 17:55:56 +0100

    Do not select 0 if base is null

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -27,7 +27,7 @@ class Configuration implements ConfigurationInterface
                         ->children()
                             ->scalarNode('host')->defaultValue('127.0.0.1')->cannotBeEmpty()->end()
                             ->integerNode('port')->defaultValue(6379)->end()
-                            ->integerNode('base')->treatNullLike(0)->defaultValue(0)->end()
+                            ->integerNode('base')->end()
                         ->end()
                     ->end()
                 ->end()
